73980bcc4fff21967746084fda9ddb3f03e1e40a
Kwinten Olaerts <olaertskwinten@gmail.com>
2019-03-14 21:34:00 +0100

    php project

---
+++ frameworks/header.php
@@ -0,0 +1,28 @@
+<head>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style type="text/css">
+    .brand{
+        background: #cbb09c !important;
+
+    }
+    .brand-text{
+        color: #cbb09c !important;
+    }
+    </style>
+    <title>248 Pizza</title>
+    <source>
+</head>
+<body class="grey lighten-4">
+<nav class="white z-depth-0">
+<div class="container">
+<a href="#" class="brand-logo brand-text">248 Quotes</a>
+<ul id="nov-mobile"> class="right hide-on-small-and-down">
+<li><a href="#" class="btn brand" z-depth-0>Voeg quote toe</a></li>
+
+</ul>
+</div>
+</nav>
+
+
+<!-- hide/add sidebar: CMD + B -->
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php include('frameworks/header.php'); ?>
+
+<?php include('frameworks/footer.php'); ?>
+
+</html>
